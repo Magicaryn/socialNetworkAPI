d1818d526d9eb0e431ae43fbdf0fe25bba91c0ed
magicary <behnke.ca@gmail.com>
2024-05-15 21:54:39 -0500

    fixed friend API

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,3 @@
-//// models need to be adjusted and this needs to change to correspond
 
 const User = require('./models/User');
 
@@ -15,7 +14,7 @@ module.exports = {
     try {
       const user = await User.findOne({ _id: req.params.userId })
         .select('-__v')
-        .populate('posts');
+        .populate('Thought');
 
       if (!user) {
         return res.status(404).json({ message: 'No user with that ID' });
@@ -36,7 +35,7 @@ module.exports = {
     }
   },
 
-  async putUser(req, res){
+  async putUser(req, res) {
     try {
       const updatedUser = await User.findByIdAndUpdate(
         req.params.userId,
@@ -52,40 +51,50 @@ module.exports = {
     }
   },
 
-  async deleteUser(req, res){
+  async deleteUser(req, res) {
     try {
-        const deletedUser = await User.findByIdAndDelete(req.params.id);
-        if (!deletedUser) {
-          return res.status(404).json({ error: 'User not found' });
-        }
-        res.json({ message: 'User deleted successfully' });
-      } catch (err) {
-        res.status(500).json(err);
+      const deletedUser = await User.findByIdAndDelete(req.params.id);
+      if (!deletedUser) {
+        return res.status(404).json({ error: 'User not found' });
       }
-    },
+      res.json({ message: 'User deleted successfully' });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
 
-    //// need to create friends and delete friends.... adding in C&P from above
-    //// simply for context but need to adjust after model is completed
+  async createFriend(req, res) {
+    try {
+      const friendId = req.body.friendId;
 
-    async createFriend(req, res) {
-      try {
-        const dbUserData = await User.create(req.body);
-        res.json(dbUserData);
-      } catch (err) {
-        res.status(500).json(err);
+      const currentUser = await User.findById(req.params.userId);
+      if (currentUser.friends.includes(friendId)) {
+        return res.status(400).json({ error: 'User is already a friend' });
       }
-    },
+      currentUser.friends.push(friendId);
+      await currentUser.save();
+      res.json(currentUser);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
 
-    async deleteFriend(req, res){
-      try {
-          const deletedUser = await User.findByIdAndDelete(req.params.id);
-          if (!deletedUser) {
-            return res.status(404).json({ error: 'User not found' });
-          }
-          res.json({ message: 'User deleted successfully' });
-        } catch (err) {
-          res.status(500).json(err);
-        }
-      },
+  async deleteFriend(req, res) {
+    try {
+      const userId = req.params.userId;
+      const friendId = req.params.friendId;
 
+      const currentUser = await User.findById(userId);
+
+      if (!currentUser.friends.includes(friendId)) {
+        return res.status(404).json({ error: 'Friend not found' });
+      }
+      currentUser.friends.pull(friendId);
+      await currentUser.save();
+      res.json({ message: 'Friend deleted successfully' });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
 };
+
